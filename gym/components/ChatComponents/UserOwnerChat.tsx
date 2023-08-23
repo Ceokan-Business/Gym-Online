13b3de69cab44fbcff62b5cbd4b1a7b99292b353
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-08-23 12:47:29 +0300

    start styling the chat

--- gym/components/ChatComponents/UserOwnerChat.tsx
+++ gym/components/ChatComponents/UserOwnerChat.tsx
@@ -2,13 +2,16 @@
 import { useState, useEffect } from 'react'; 
 import { MessageInterface } from '@interfaces/MessageInterface';
 
+import { RiSendPlaneFill } from 'react-icons/ri'; 
+
 interface Props { 
   userId: string 
 }
 
 const UserOwnerChat = ({ userId }: Props) => {
   const [ userMessages, setUserMessages ] = useState <MessageInterface []> ([]); 
-
+  const [ newText, setNewText ] = useState<string> (""); 
+  
   useEffect( () => { 
     const getMessageData = async () => { 
       const response = await fetch(`/api/users/${userId}/messages`); 
@@ -16,10 +19,27 @@ const UserOwnerChat = ({ userId }: Props) => {
 
       setUserMessages(messageResponse); 
     }
-  })
+  }); 
+
+  const sendNewMessage = async () => { 
+    try { 
+
+    } catch(err) { 
+      console.log(err); 
+    }
+   }
   return (
-      <div className="fixed bottom-0 right-0 h-1/2 w-1/4 bg-light-yellow">
+      <div className="fixed bottom-6 right-0 h-1/2 w-1/4 bg-light-yellow">
         <p> { userId } </p>
+        <form className = 'absolute inset-x-0 bottom-3 h-8 flex justify-center' onSubmit = { sendNewMessage }>
+          <input type="text" value = { newText } onChange = { (e) => { setNewText(e.target.value)}} />
+          <button type = 'submit'>
+            { newText === '' ? 
+              <RiSendPlaneFill className = 'text-gray-500 text-3xl'/> : 
+              <RiSendPlaneFill className = 'text-black text-3xl'/> 
+            }
+          </button>
+        </form>
       </div>
   );
 };
